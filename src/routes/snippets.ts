6a297c103e5893095c7b27e82874881fe03e4db1
shivain2124 <shivainsharma2124@gmail.com>
2025-05-25 01:51:43 +0530

    Refactor snippet routes to enforce authentication middleware for all operations

--- src/routes/snippets.ts
+++ src/routes/snippets.ts
@@ -1,18 +1,27 @@
 import express,{ Router, Request, Response } from "express";
 import {getAllSnippets,addSnippet,getById,deleteById,updateById} from "../controllers/snippetController"
 import {validateSnippet} from "../middleware/validation";
+import { authMiddleware } from "../middleware/authMiddleware"
 
 const router=Router();
+// isPublic: true -> remember
 
-type SnippetInput={
-    title:string;
-    code:string;
-};
+// //public routes
+// router.get('/',getAllSnippets);
+// router.get('/:id',getById);
 
-router.get('/',getAllSnippets);
-router.post('/',validateSnippet,addSnippet);
-router.get('/:id',getById);
-router.delete('/:id',deleteById);
-router.put('/:id',validateSnippet,updateById);
+// //prtotected routes
+// router.post('/',authMiddleware,validateSnippet,addSnippet);
+// router.delete('/:id',authMiddleware,deleteById);
+// router.put('/:id',authMiddleware,validateSnippet,updateById);
+
+router.use(authMiddleware);
+
+router.get("/", getAllSnippets);
+router.get("/:id", getById);
+router.post("/", validateSnippet, addSnippet);
+router.put("/:id", validateSnippet, updateById);
+router.delete("/:id", deleteById);
+
 
 export default router;
