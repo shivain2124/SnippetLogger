35de46b87bb1e363333ba42f13e4eccc8fefaeba
shivain2124 <shivainsharma2124@gmail.com>
2025-05-25 01:51:33 +0530

    Add createdBy field to codeSnippet schema to associate snippets with users

--- src/models/codeSnippet.ts
+++ src/models/codeSnippet.ts
@@ -14,6 +14,11 @@ const codeSnippetSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true } 
 );
